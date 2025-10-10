bd85e9b378f126905d3250b7996580f413c59836
MdNisar1 <mdnisarktr62@gmail.com>
2025-10-11 05:09:34 +0530

    2nd fixed

--- src/Pages/Home/HomePages.jsx
+++ src/Pages/Home/HomePages.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Home from '../../components/home/home'
-import Navbar from '../../components/navbar/navbar'
+
 import About from '../../components/About/about'
 import Footer from '../../components/Footer/footer'
 import Project from '../../components/Projects/project'
 import Contact from '../../components/Contact/contact'
 import Skills from '../../components/Skills/skills'
+import Navbar from '../../components/Navbar/navbar'
+import Home from '../../components/Home/Home'
 
 
 const HomePages = () => {
