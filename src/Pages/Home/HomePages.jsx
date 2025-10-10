495abe42cd0f32b6926380fd782f34d36953af42
MdNisar1 <mdnisarktr62@gmail.com>
2025-10-11 05:17:37 +0530

    third fix

--- src/Pages/Home/HomePages.jsx
+++ src/Pages/Home/HomePages.jsx
@@ -4,7 +4,7 @@ import About from '../../components/About/about'
 import Footer from '../../components/Footer/footer'
 import Project from '../../components/Projects/project'
 import Contact from '../../components/Contact/contact'
-import Skills from '../../components/Skills/skills'
+import Skills from '../../components/Skills/Skills'
 import Navbar from '../../components/Navbar/navbar'
 import Home from '../../components/Home/Home'
 
