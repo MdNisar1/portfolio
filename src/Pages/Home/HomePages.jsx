6f3b107ea5589d18f1922bab6d2397de2fdfcc01
MdNisar1 <mdnisarktr62@gmail.com>
2026-01-16 02:16:15 +0530

    Fix Home component import path

--- src/Pages/Home/HomePages.jsx
+++ src/Pages/Home/HomePages.jsx
@@ -5,8 +5,9 @@ import Footer from '../../components/Footer/footer'
 import Project from '../../components/Projects/project'
 import Contact from '../../components/Contact/contact'
 import Skills from '../../components/Skills/Skills'
-import Navbar from '../../components/Navbar/navbar'
-import Home from '../../components/Home/Home.jsx'
+import Navbar from '../../../src/components/Navbar/navbar.jsx'
+import Home from '../../../src/components/Home/Home.jsx';
+
 import Education from '../../components/Education/education'
 
 
