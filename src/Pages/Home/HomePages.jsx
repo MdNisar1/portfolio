4b149901f5d4de6b9e91306e4d913ce57dd5c0f2
MdNisar1 <mdnisarktr62@gmail.com>
2026-01-16 22:23:33 +0530

    Fix HomePages import paths for Netlify build

--- src/Pages/Home/HomePages.jsx
+++ src/Pages/Home/HomePages.jsx
@@ -5,9 +5,8 @@ import Footer from '../../components/Footer/footer'
 import Project from '../../components/Projects/project'
 import Contact from '../../components/Contact/contact'
 import Skills from '../../components/Skills/Skills'
-import Navbar from '../../../src/components/Navbar/navbar.jsx'
-import Home from '../../../src/components/Home/Home.jsx';
-
+import Navbar from '../../components/Navbar/navbar.jsx'
+import Home from "../../components/Home/Home.jsx";
 import Education from '../../components/Education/education'
 
 
