916e53c0e8bf96f4ea1e299984d7e011464aa0a2
MdNisar1 <mdnisarktr62@gmail.com>
2026-01-17 15:26:12 +0530

    remove semi colon

--- src/Pages/Home/HomePages.jsx
+++ src/Pages/Home/HomePages.jsx
@@ -6,7 +6,7 @@ import Project from '../../components/Projects/project'
 import Contact from '../../components/Contact/contact'
 import Skills from '../../components/Skills/Skills'
 import Navbar from '../../components/Navbar/navbar.jsx'
-import Home from "../../components/Home/Home.jsx";
+import Home from "../../components/Home/Home.jsx"
 import Education from '../../components/Education/education'
 
 
